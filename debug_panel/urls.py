efdedc9859f2d31fd92f5de0ea9a49d47675c391
sambone <sbolgert@telemetryfi.com>
2024-02-05 13:32:54 -0600

    Fix urls

--- debug_panel/urls.py
+++ debug_panel/urls.py
@@ -7,9 +7,12 @@ debug-panel application.
 from .views import debug_data
 
 try:
-    from django.conf.urls import url
-except ImportError:  # django < 1.4
-    from django.conf.urls.defaults import url
+    from django.urls import path as url
+except ImportError:
+    try:
+        from django.conf.urls import url
+    except ImportError:  # django < 1.4
+        from django.conf.urls.defaults import url
 
 _PREFIX = '__debug__'
 
